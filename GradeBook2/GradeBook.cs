740414cffcbd86dcfb2956de20c24b933f3cd718
Nuru-rahman Akinpelu <steadyrahman1@gmail.com>
2022-05-11 12:37:04 +0100

    GradeBookcommit

---
+++ GradeBook2/GradeBook.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+public class GradeBook
+{
+    public void DisplayMessage( string courseName)
+    {
+        Console.WriteLine("Welcome to the gradebook for \n{0}!", courseName);
+    }
+}
+
+
---
+++ GradeBook2/GradeBookTest.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+public class GradeBookTest
+{
+    public static void Main(string[] argh)
+    {
+        GradeBook myGradeBook = new GradeBook();
+
+        Console.WriteLine("Please enter the coursename:");
+        string nameOfCourse = Console.ReadLine();
+        Console.WriteLine();
+
+
+        myGradeBook.DisplayMessage(nameOfCourse);
+    }
+}
